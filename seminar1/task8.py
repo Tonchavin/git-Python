7f0e01041a71e6d67d156d09ac18759782f0e96d
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-03-12 08:23:58 +1000

    add seminar1

--- seminar1/task8.py
+++ seminar1/task8.py
@@ -7,3 +7,14 @@
 3 2 4 -> yes
 3 2 1 -> no
 """
+print('Введите размеры шоколадной плитки: ')
+rows = int(input())
+columns = int(input())
+cut_count = int(input('Введите сколько хотите отломить плиток шоколада: '))
+
+if (rows == 1 or columns == 1) and (cut_count < rows or cut_count < columns) and (rows != columns):
+    print(f'Вы можете {cut_count} отломить')
+if cut_count > 1 and cut_count % 2 == 0:
+    print(f'Вы можете {cut_count} отломить')
+else:
+    print(f'Вы не можете {cut_count} отломить')
