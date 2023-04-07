c5e020a9896b9ba9b3520334d09099dcee8a55a7
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-04-07 17:32:07 +1000

    add fix_3 seminar8

--- seminar8/task38.py
+++ seminar8/task38.py
@@ -6,6 +6,7 @@ import shutil
 print('Меню тел.справочника: \n1 - Показать тел.справочник \n2 - Добавить новую запись в тел.справочник '
       '\n3 - Редактировать тел.справочник \n4 - Поиск в тел.справочнике \n5 - Удалить запись из тел.справочника')
 
+
 # src = 'F:\Less_CZN\git-Python\seminar8\phone.txt'
 # dest = 'F:\Less_CZN\git-Python\seminar8\phone_copy.txt'
 # shutil.copy2(src, dest)
@@ -35,7 +36,7 @@ def in_fix_line_phone():
     file_phone = open('F:\Less_CZN\git-Python\seminar8\phone.txt', 'r+', encoding='UTF-8')
     phone = file_phone.readlines()
     file_phone.close()
-    print(*[item + '\n' for item in phone])
+    # print(*[item + '\n' for item in phone])
     find_word = input('Введите поисковые данные через пробел: ')
     in_index_line = []
     for in_find_word in phone:
@@ -45,7 +46,6 @@ def in_fix_line_phone():
             print(f'Найдено: ', in_find_word)
             print('Номер строки', phone.index(in_find_word))
             in_index_line.append(phone.index(in_find_word))
-    # print(in_index_line)
     index_line = int(input('Введите номер строки которую хотите изменить: '))
     print(phone[index_line])
     new_line = input('Введите новую запись через пробел(Phone FIO Address House Corps Room): ').split()
@@ -72,7 +72,25 @@ def in_find_phone():
     file_phone.close()
 
 
-# def in_del_line_phone():
+def in_del_line_phone():
+    file_phone = open('F:\Less_CZN\git-Python\seminar8\phone.txt', 'r+', encoding='UTF-8')
+    phone = file_phone.readlines()
+    file_phone.close()
+    find_word = input('Введите поисковые данные через пробел: ')
+    in_index_line = []
+    for in_find_word in phone:
+        if find_word in in_find_word:
+            print('          Phone|          FIO           |Address|House|Corps|Room|')
+            print('          ________________________________________________________')
+            print(f'Найдено: ', in_find_word)
+            print('Номер строки', phone.index(in_find_word))
+            in_index_line.append(phone.index(in_find_word))
+    index_line = int(input('Введите номер строки которую хотите удалить: '))
+    print(phone[index_line])
+    phone.remove(phone[index_line])
+    file_phone = open('F:\Less_CZN\git-Python\seminar8\phone.txt', 'w', encoding='UTF-8')
+    file_phone.write(''.join(map(str, phone)))  # запись в файл с новой строки
+    file_phone.close()
 
 
 number_main_phone = int(input('Введите нужную цифру: '))
